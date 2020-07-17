08a92f0f390270babe2382fd09c65a8c8e847253
Kimberly Atalya <kimberlyatte@yahoo.com>
2020-07-17 12:48:29 +0700

    fifthCommit

--- go.mod
+++ go.mod
@@ -1,12 +1,13 @@
 module back_end
+
 // +heroku goVersion go1.14
 
 go 1.14
 
-
 require (
 	github.com/99designs/gqlgen v0.11.3
+	github.com/go-chi/chi v3.3.2+incompatible
 	github.com/go-pg/pg/v10 v10.0.0-beta.6
-	github.com/graphql-go/graphql v0.7.9 // indirect
+	github.com/rs/cors v1.6.0
 	github.com/vektah/gqlparser/v2 v2.0.1
 )
--- server.go
+++ server.go
@@ -10,28 +10,47 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "5000"
 
 func main() {
-	db := pg.Connect(&pg.Options{
-		Addr:     ":5432",
-		User:     "postgres",
-		Password: "kimkim12",
-		Database: "YouRJube",
-	})
+	//db := pg.Connect(&pg.Options{
+	//	Addr:     ":5432",
+	//	User:     "postgres",
+	//	Password: "kimkim12",
+	//	Database: "YouRJube",
+	//})
+
+	opt, err := pg.ParseURL("postgres://jtgveredqlibys:fa1a765b8c1cea74015149327a79276d64fbe8b91c17dd2a6ba79b45f7bf814c@ec2-54-234-28-165.compute-1.amazonaws.com:5432/d9knci00ltr1dh?sslmode=require")
+	if err != nil {
+		panic(err)
+	}
+
+	db := pg.Connect(opt)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
+	router := chi.NewRouter()
+
+	// Add CORS middleware around every request
+	// See https://github.com/rs/cors for full option listing
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:8080"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{DB: db}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
