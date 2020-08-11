70d5d6fb6806d6bc945377d0465eb1d59e9125a2
Kimberly Atalya <kimberlyatte@yahoo.com>
2020-08-11 09:35:20 +0700

    11Aug2020(5)

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -568,7 +568,7 @@ func (r *queryResolver) GetVideoHomePage(ctx context.Context, restriction string
 
 	var video_res []*model.Video
 
-	err_pr := r.DB.Model(&vid).Where("video_premium IS NOT FALSE").Select()
+	err_pr := r.DB.Model(&vid).Where("video_premium = false").Select()
 
 	if err_pr != nil {
 		log.Println(err_pr)
